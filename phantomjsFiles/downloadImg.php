065596be4dbb59c6167f15d30800238400249793
hana arib <biranah@gmail.com>
2015-03-17 17:49:04 +0000

    fichier 'downloadImg.php' modifié, le fichier lu contient seulement les src des images

--- phantomjsFiles/downloadImg.php
+++ phantomjsFiles/downloadImg.php
@@ -1,12 +1,12 @@
 <?php
-$data = file_get_contents("imgs.txt"); //read the file
-$convert = explode("\r", $data); //create array separate by new line
+$data = file_get_contents('img_urls.txt'); //read the file
+$convert = explode(',', $data); //create array separate by new line
 
 for ($i=0;$i<count($convert);$i++)  
 {
 	echo $convert[$i];
 	//var_dump($convert[$i]);
-    download_remote_file(trim($convert[$i]),'image'.$i.'.jpg'); //write value by index
+    download_remote_file($convert[$i],'image'.$i.'.jpg'); //write value by index
 }
 
 	function download_remote_file($file_url, $save_to)
