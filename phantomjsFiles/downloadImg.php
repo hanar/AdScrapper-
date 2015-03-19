11f31ca6547be89b1afcd7e2a400aa4e50143b1f
hana arib <biranah@gmail.com>
2015-03-19 09:50:26 +0000

    exec_cmd : petit script php qui permet d'executer le script d'extraction sur plusieurs sites

--- phantomjsFiles/downloadImg.php
+++ phantomjsFiles/downloadImg.php
@@ -1,19 +1,18 @@
 <?php
-$data = file_get_contents('img_urls.txt'); //read the file
-$convert = explode(',', $data); //create array separate by new line
 
-for ($i=0;$i<count($convert);$i++)  
-{
-	echo $convert[$i];
-	//var_dump($convert[$i]);
-    download_remote_file($convert[$i],'image'.$i.'.jpg'); //write value by index
+function download_remote_file($file_url, $save_to) {
+	$content = file_get_contents($file_url);
+	file_put_contents($save_to, $content);
+}
+$data = file_get_contents('img_urls1.txt'); //read the file
+$result = json_decode($data);
+$i=0;
+foreach($result as $image){
+	
+    download_remote_file($image->img,'hespress'.$i.'.jpg'); //write value by index
+    $i=$i+1;
 }
 
-	function download_remote_file($file_url, $save_to)
-	{
-		$content = file_get_contents($file_url);
-		file_put_contents($save_to, $content);
-	}
 ?>
 
 
