48d46874295209e1a9f7bda4babb4332e8aaf594
hana arib <biranah@gmail.com>
2015-04-02 16:47:48 +0100

    fontion adwalk_page ajoutée

--- utils/functions.js
+++ utils/functions.js
@@ -1,6 +1,6 @@
 /*
 Fonction qui parse une url et retourne le hostname
-
+N'est pas utilisé pour le moment
 */
 function adwalk_parseURL(url) {
     var parser = document.createElement('a');
@@ -9,35 +9,47 @@ function adwalk_parseURL(url) {
     return parser.hostname;
 };
 
-/*Fonction 'adwalk_getHostname' qui prend une URL en param et
-retourne le nom de domaine
-Param: url --> String
-*/
-
 function adwalk_getHostname() {
-    return $(location).attr('hostname').substr(4);
+    return $(location).attr('hostname');
+    
+}
+/*Fonction 'adwalk_getHostname' retourne le hostname de la page courante
+N'est pas utilisé pour le moment
+ */
+
+function adwalk_getHostnames(hostnames) {
+    //return $(location).attr('hostname');
+    //On enleve le premier élément du tableau, vu que le tableau passé 
+    //en param est selon le format: urlDeLaPage,hostname1,hostname2..
+    return hostnames.splice(0, 1);
 }
 
-//Fonctions: 
-/* fonction'adwalk_testExterneURL'
+/* Fonction'adwalk_testExterneURL'
 Teste si une URL ne correspond pas au nom du domaine courant
 Param : url--> String; hostname --> String
 */
 function adwalk_isExternalURL(url, hostnames) {
         var parser = document.createElement('a');
         parser.href = url;
-        // parser.hostname; // => "example.com"
-        host = parser.hostname;
-        //  host = hostname.substr(4);
-        for (var i = hostnames.length - 1; i >= 0; i--) {
-            
-            if (host.indexOf(hostnames[i]) == -1) {
-                return true;
-            } else {
-                return false;
-            };
-        };
+        host = adwalk_getDomainName(parser.hostname);
+        console.log(hostnames);
+        if ($.isArray(hostnames)) {
+            $.each(hostnames, function(index, value) {
+                console.log(host + '--' + value);
+                if (host != adwalk_getDomainName(value)) {
+                    return true;
+                }
+            });
+            return false;
+        } else {
 
+            console.log("not an array  " + host + '---' + adwalk_getDomainName(hostnames));
+            if (host == adwalk_getDomainName(hostnames)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
     }
     /*Fonction 'adwalk_getSrc'
     Récupère le src d'un tag 'img' et de son parent 'a'.
@@ -45,13 +57,54 @@ function adwalk_isExternalURL(url, hostnames) {
     Param : img --> String: tag img; hostname -->String
     Retourne un objet JSON
     */
-function adwalk_getSrc(img, hostname) {
-    var url = img.parent('a').attr('href');
-    var src = img.attr('src');
-    if (adwalk_testExterneURL(src, hostname)) {
-        return ({
-            "src": src,
-            "url": url
-        });
+function adwalk_getSrc(img, hostnames) {
+        hostnames = adwalk_getHostnames(hostnames);
+        console.log(hostnames);
+        var url = img.parent('a').attr('href');
+        var src = img.attr('src');
+        if (adwalk_isExternalURL(src, hostnames)) {
+            return ({
+                "src": src,
+                "url": url
+            });
+        } else return false;
     }
+    /*Fonction adwalk_getDomainName 
+    Retourne le domain à partir du hostname
+    Param : hostname--> String
+    Retour: sndleveldomain --> String*/
+function adwalk_getDomainName(hostname) {
+    var parts = hostname.split('.');
+    if (parts.length == 3) {
+        sndleveldomain = parts.slice(1).join('.');
+        return sndleveldomain;
+    } else if (parts.length <= 3) {
+        sndleveldomain = parts.slice(0).join('.');
+        return sndleveldomain;
+    }
+}
+
+function adwalk_page(hostnames) {
+    images = [];
+
+    //Récupérer les hostnames avec lesquels on comparera le src de l'image
+    hostnames = adwalk_getHostnames(hostnames);
+    console.log('hostnames:' + hostnames);
+    $('iframe').each(function() {
+        console.log($(this).contents().find('a img').length);
+        $(this).contents().find('a img').each(function() {
+            //Appel de la fonction getSrc
+            result = adwalk_getSrc($(this), hostnames);
+            if (result)
+                images.push(result);
+
+        });
+    });
+
+    $('a img').each(function() {
+        result = adwalk_getSrc($(this), hostnames);
+        if (result) images.push(result);
+    });
+
+    return images;
 }
